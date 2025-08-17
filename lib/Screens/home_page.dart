5dc55c6d57cdd998e97d8d3988367ccc953498bd
BENSAMEH <ahmedsameh77sa@gmail.com>
2025-08-18 00:15:48 +0300

    change the sign in and sign up to typerAnimatedText

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
 import 'package:login_ui_firebase_auth/Screens/categories_screen.dart';
 import 'package:login_ui_firebase_auth/Screens/home_screen.dart';
 import 'package:login_ui_firebase_auth/Screens/profile_screen.dart';
@@ -13,19 +14,22 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int currentIndex = 1;
-  List screens=[ProfileScreen(),HomeScreen(),CategoriesScreen()];
+  List screens = [ProfileScreen(), HomeScreen(), CategoriesScreen()];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: screens[currentIndex],
+    return Scaffold(
+      body: screens[currentIndex],
       bottomNavigationBar: BottomNavigationBar(
-        selectedLabelStyle: TextStyle(
+        elevation: 0,
+        selectedLabelStyle: GoogleFonts.rubik(
           color: Colors.white,
           fontWeight: FontWeight.bold,
         ),
+        iconSize: 35,
         selectedItemColor: Colors.white,
         unselectedItemColor: Colors.grey.shade700,
-        unselectedLabelStyle: TextStyle(fontWeight: FontWeight.w500),
+        unselectedLabelStyle: GoogleFonts.rubik(fontWeight: FontWeight.w500),
         currentIndex: currentIndex,
         backgroundColor: Color(0xFFFF8383),
         onTap: (value) {
@@ -34,10 +38,9 @@ class _HomePageState extends State<HomePage> {
           });
         },
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: "profile"),
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
-          BottomNavigationBarItem(icon: Icon(Icons.menu), label: "categories"),
-
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.menu), label: ""),
         ],
       ),
     );
